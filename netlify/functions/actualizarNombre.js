8d9567442a81a1d78b653a751efc22958c12f60a
meeeww <thezasm03@gmail.com>
2023-08-16 20:25:25 +0200

    Creacion de ids riot

---
+++ netlify/functions/actualizarNombre.js
@@ -0,0 +1,30 @@
+import { schedule } from "@netlify/functions";
+import axios from 'axios'
+
+import { Webhook } from 'discord-webhook-node';
+const hook = new Webhook("https://discord.com/api/webhooks/1141434977733582900/XLZWqEsQti3PwOhxsFDXHKxU_owbh3L11iUcn0cHbl5yIlSjlUvBmu598HivoOOKdSi2");
+
+const API = "RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206"
+
+
+const handler = async function (event, context) {
+    await axios.get("https://api.chaoschampionship.com/.netlify/functions/api/usuarios").then(async function (response1) {
+        if (response1.status == 200) {
+            for (let cuenta in response1.data) {
+                if (response1.data[cuenta]["id_ingame"] == null) {
+                    await axios.get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + response1.data[cuenta]["nombre_ingame"] + "?api_key=" + API).then(async function (response2) {
+                        await axios.put("https://api.chaoschampionship.com/.netlify/functions/api/usuarios/modificar/lol/ids", { idCuenta: response1.data[cuenta]["id_cuenta"], idRiot: response2.data["id"], puuidRiot: response2.data["puuid"] }, { timeout: 10000, headers: { 'Content-Type': 'application/json' } })
+                    })
+                } else {
+                    await axios.get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + response1.data[cuenta]["puuid_ingame"] + "?api_key=" + API).then(async function (response2) {
+                        if (response1.data[cuenta]["nombre_ingame"] != response2.data["name"]) {
+                            await axios.put("https://api.chaoschampionship.com/.netlify/functions/api/usuarios/modificar/lol/nombre", { nombreRiot: response2.data["name"], idUsuario: response1.data[cuenta]["id_usuario"] }, { timeout: 10000, headers: { 'Content-Type': 'application/json' } })
+                        }
+                    })
+                }
+            }
+        }
+    })
+};
+
+exports.handler = schedule("0 * * * *", handler);
