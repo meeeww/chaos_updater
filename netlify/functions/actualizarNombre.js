b65f6bddaf993d2606add10f0901891d8111687e
Zas <102923191+meeeww@users.noreply.github.com>
2023-10-18 18:33:41 +0200

    Cambio de iconos

--- netlify/functions/actualizarNombre.js
+++ netlify/functions/actualizarNombre.js
@@ -1,5 +1,7 @@
 import { schedule } from "@netlify/functions";
+
 import axios from 'axios'
+import directorio from "../../variables.json"
 
 import { Webhook } from 'discord-webhook-node';
 const hook = new Webhook("https://discord.com/api/webhooks/1141434977733582900/XLZWqEsQti3PwOhxsFDXHKxU_owbh3L11iUcn0cHbl5yIlSjlUvBmu598HivoOOKdSi2");
@@ -8,7 +10,7 @@ const API = "RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206"
 //https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/SupportConPanza?api_key=RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206
 
 const handler = async function (event, context) {
-    await axios.get("https://api.chaoschampionship.com/.netlify/functions/api/usuarios").then(async function (response1) {
+    await axios.get(directorio.directorio + "usuarios").then(async function (response1) {
         if (response1.status == 200) {
             for (let usuario in response1.data) {
                 try {
