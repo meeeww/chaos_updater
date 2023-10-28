0203187815a9830119b72ecfb033f1a9264827ce
Zas <102923191+meeeww@users.noreply.github.com>
2023-10-28 03:09:04 +0200

    debug nombre

--- netlify/functions/actualizarFotoPerfil.js
+++ netlify/functions/actualizarFotoPerfil.js
@@ -4,51 +4,74 @@ import axios from "axios";
 import directorio from "../../variables.json";
 
 import { Webhook } from "discord-webhook-node";
-const hook = new Webhook(
-  "https://discord.com/api/webhooks/1141434977733582900/XLZWqEsQti3PwOhxsFDXHKxU_owbh3L11iUcn0cHbl5yIlSjlUvBmu598HivoOOKdSi2"
-);
+const hook = new Webhook("https://discord.com/api/webhooks/1141434977733582900/XLZWqEsQti3PwOhxsFDXHKxU_owbh3L11iUcn0cHbl5yIlSjlUvBmu598HivoOOKdSi2");
 
 const API = "RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206";
 //https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/SupportConPanza?api_key=RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206
 
 const handler = async function (event, context) {
-  console.log(directorio.directorio + "usuarios", { headers: { "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf" } })
-  await axios.get(directorio.directorio + "usuarios", { headers: { "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf" } }).then(async function (response1) {
-    if (response1.status == 200) {
-      for (let usuario in response1.data) {
-        try {
-          await axios
-            .get(directorio.directorio + "usuarios/cuentas/id=" + response1["data"][usuario]["id_usuario"], { headers: { "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf" } })
-            .then(async function (response2) {
-              if (response2.data.length > 0) {
-                await axios
-                  .get(
-                    "https://europe.api.riotgames.com/riot/account/v1/accounts/by-puuid/" +
-                      response2.data[0]["puuid_lol"] +
-                      "?api_key=" +
-                      API
-                  )
-                  .then(async function (response3) {
-                    if (response3.status != 404) {
-                      await axios.put(directorio.directorio + "usuarios/icono", {
-                        id: response1["data"][usuario]["id_usuario"],
-                        icono: response3.data["profileIconId"].toString()
-                      },
-                      { headers: { "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf" } });
+    console.log(directorio.directorio + "usuarios", {
+        headers: {
+            "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf",
+        },
+    });
+    await axios
+        .get(directorio.directorio + "usuarios", {
+            headers: {
+                "x-auth-token":
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf",
+            },
+        })
+        .then(async function (response1) {
+            if (response1.status == 200) {
+                for (let usuario in response1.data) {
+                    try {
+                        await axios
+                            .get(directorio.directorio + "usuarios/cuentas/id=" + response1["data"][usuario]["id_usuario"], {
+                                headers: {
+                                    "x-auth-token":
+                                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf",
+                                },
+                            })
+                            .then(async function (response2) {
+                                if (response2.data.length > 0) {
+                                    await axios
+                                        .get(
+                                            "https://europe.api.riotgames.com/riot/account/v1/accounts/by-puuid/" +
+                                                response2.data[0]["puuid_lol"] +
+                                                "?api_key=" +
+                                                API
+                                        )
+                                        .then(async function (response3) {
+                                            if (response3.status != 404) {
+                                                await axios.put(
+                                                    directorio.directorio + "usuarios/icono",
+                                                    {
+                                                        id: response1["data"][usuario]["id_usuario"],
+                                                        icono: response3.data["profileIconId"].toString(),
+                                                    },
+                                                    {
+                                                        headers: {
+                                                            "x-auth-token":
+                                                                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf",
+                                                        },
+                                                    }
+                                                );
+                                            }
+                                        });
+                                } else {
+                                }
+                            });
+                    } catch (e) {
+                        console.log(e);
+                        hook.send("Fallo en la función actualizar foto perfil. <@286402429258301440>");
                     }
-                  });
-              } else {
-              }
-            });
-        } catch (e) {
-          console.log(e);
-          hook.send("Fallo en la función actualizar foto perfil. <@286402429258301440>");
-        }
-      }
-    }
-  }).catch(async function (error) {
-    console.log(error)
-  });
+                }
+            }
+        })
+        .catch(async function (error) {
+            console.log(error);
+        });
 };
 
 exports.handler = schedule("*/5 * * * *", handler);
--- netlify/functions/actualizarNombre.js
+++ netlify/functions/actualizarNombre.js
@@ -10,6 +10,11 @@ const API = "RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206";
 //https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/SupportConPanza?api_key=RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206
 
 const handler = async function (event, context) {
+    console.log(directorio.directorio + "usuarios", {
+        headers: {
+            "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf",
+        },
+    });
     await axios
         .get(directorio.directorio + "cuentas", {
             headers: {
@@ -96,6 +101,9 @@ const handler = async function (event, context) {
                     }
                 }
             }
+        })
+        .catch(async function (error) {
+            console.log(error);
         });
 };
 
