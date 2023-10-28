8fe170892166c3d781b6e32748b56dec34b23be6
Zas <102923191+meeeww@users.noreply.github.com>
2023-10-28 03:30:32 +0200

    debug 5

--- netlify/functions/actualizarNombre.js
+++ netlify/functions/actualizarNombre.js
@@ -19,8 +19,8 @@ const handler = async function (event, context) {
         })
         .then(async function (response1) {
             if (response1.status == 200) {
-                console.log(response1.data)
-                for (let cuenta in response1.data.data) {
+                //console.log(response1.data)
+                for (let cuenta in response1.data.result) {
                     console.log(response1["data"]["result"]);
                     console.log(response1["data"]["result"][cuenta]);
                     console.log(cuenta)
