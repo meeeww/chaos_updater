8070c13c7a9aa1813eafef3dd909b2c979362262
Christian Toca <ctmsp98@gmail.com>
2023-12-05 20:08:13 +0100

    no message

--- functions/actualizarFotoPerfil.js
+++ functions/actualizarFotoPerfil.js
@@ -69,56 +69,12 @@ export default async function actualizarFotoPerfil() {
             console.log(user)
             //console.log(e.response);
             
-            //if (e.response.status != 404) hook.send("Fallo en la función actualizar foto perfil. <@286402429258301440>");
+            if (e.response.status != 404) hook.send("Fallo en la función actualizar foto perfil. <@286402429258301440>");
           }
         });
-
-        // for (let usuario in response1.data.result) {
-        //   //console.log(usuario)
-        //   try {
-        //     if (response1["data"]["result"][usuario]["icono"] == 0) {
-        //       await axios
-        //         .get(directorio.directorio + "usuarios/cuentas/id=" + response1["data"]["result"][usuario]["id_usuario"], {
-        //           headers: {
-        //             "x-auth-token":
-        //               "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf4",
-        //           },
-        //         })
-        //         .then(async function (response2) {
-        //           if (response2.data.result.length > 0) {
-        //             await axios
-        //               .get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/" + response2.data.result[0]["puuid_lol"] + "?api_key=" + API)
-        //               .then(async function (response3) {
-        //                 if (response3.status == 200) {
-        //                   await axios.put(
-        //                     directorio.directorio + "usuarios/icono",
-        //                     {
-        //                       id: response1["data"]["result"][usuario]["id_usuario"],
-        //                       icono: response3.data["profileIconId"].toString(),
-        //                     },
-        //                     {
-        //                       headers: {
-        //                         "x-auth-token":
-        //                           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTIsInJvbCI6MjAsImlhdCI6MTY5ODE3OTA4N30.B0jQsHr758WzdB7Vv50q-kMHQoNlHVQvwq5E6Wpuvf4",
-        //                       },
-        //                     }
-        //                   );
-        //                 } else {
-        //                   hook.send("Fallo en la función actualizar foto perfil. <@286402429258301440> Error:" + response1.status);
-        //                 }
-        //               });
-        //           }
-        //         });
-        //     }
-        //   } catch (e) {
-        //     //console.log(e);
-        //     console.log("error")
-        //     if (e.response.status != 404) hook.send("Fallo en la función actualizar foto perfil. <@286402429258301440>");
-        //   }
-        // }
       }
     })
     .catch(async function (error) {
-    console.log(error);
+      console.log(error);
     });
 };
--- index.js
+++ index.js
@@ -6,20 +6,20 @@ import actualizarNombre from "./functions/actualizarNombre.js";
 import conseguirEstadisticas from "./functions/conseguirEstadisticas.js"
 
 
-  console.log("Actualizando foto de perfil.")
+  // console.log("Actualizando foto de perfil.")
+  // actualizarFotoPerfil();
+cron.schedule("*/5 * * * *", () => {
+  console.log("Actualizando fotos de perfil.")
   actualizarFotoPerfil();
-// cron.schedule("*/5 * * * *", () => {
-//   console.log("Actualizando foto de perfil.")
-//   actualizarFotoPerfil();
-// });
+});
 
-// cron.schedule("0 7 * * *", () => {
-//   actualizarFotoPerfilForzado();
-// });
+cron.schedule("0 7 * * *", () => {
+  actualizarFotoPerfilForzado();
+});
 
-// cron.schedule("*/3 * * * *", () => {
-//   actualizarNombre();
-// });
+cron.schedule("*/3 * * * *", () => {
+  actualizarNombre();
+});
 
 cron.schedule("*/2 * * * * *", () => {
   //console.log("hey")
